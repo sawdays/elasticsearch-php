bd610e7c96c680dc5f0da9f56c8597f71245384c
Matt Stone <matt.stone@atomicsmash.co.uk>
2024-04-17 16:00:13 +0100

    Added support for PHP 8.1+ which involved updating a few packages and removing sami/sami as it's been abandoned

--- travis/generate_docs.sh
+++ travis/generate_docs.sh
@@ -1,1 +1,2 @@
-php vendor/sami/sami/sami.php update util/DocsConfig.php
+# php vendor/sami/sami/sami.php update util/DocsConfig.php
+echo "sami/sami package is not compatible with PHP 7.2+ so has been removed and not replaced so API docs will be out of date
